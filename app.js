0beb866d27e796bc9b8d7b008c2aaa9aff409d9e
Vlad Ştepu <vlad@wonsulting.com>
2025-10-27 09:13:37 +0200

    node last lesson

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const _ = require('lodash')
 
-const items = [1,[2,[3,[4]]]]
-const newItem = _.flattenDeep(items)
-console.log('newItem: ', newItem);
+console.log('something');
+// function to save the lesson 
+//const store = require('./store.js')
+//store.saveLesson('npm.js')
+//
---
+++ npm.js
@@ -0,0 +1,9 @@
+// npm init -y  -to create package.json
+// npm i lodash  -to install lodash package
+// npm i nodemon -D  -to install nodemon
+// npm -v -to see npm version
+
+// function to save the lesson 
+const store = require('./store.js')
+store.saveLesson('npm.js')
+//
